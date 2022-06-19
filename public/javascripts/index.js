ab98411af946b131931fad1cdb5756ca8f5562fb
baeksujeong <sujeongb815@gmail.com>
2022-06-20 03:17:56 +0900

    [add]test

---
+++ public/javascripts/index.js
@@ -0,0 +1,114 @@
+const clock = document.querySelector("h2#clock")
+
+function getClock(){
+    const date = new Date();
+    
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText =`${hours}:${minutes}:${seconds}`
+}
+function changeBg(){
+    const images = ["bg1.jpg", "bg2.jpg","bg3.jpg","bg4.png"];
+    const RandomImages = images[Math.floor(Math.random() * images.length)];
+    document.body.style.backgroundImage = `url('./images/${RandomImages}')`
+}
+
+getClock()
+changeBg()
+setInterval(getClock, 1000)
+
+const API_KEY = "fe63c14e98b8f3d6a9042440c2530112"
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`
+    console.log(url)
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child")
+        const city = document.querySelector("#weather span:last-child")
+        city.innerText =  data.name
+        weather.innerText =  `${data.weather[0].main}`
+    })
+}
+function onGeoError(){
+    alert("Can't find you. No weather for you")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError)
+
+const idForm = document.querySelector("#name")
+const idinput = idForm.querySelector("input")
+const userName = document.querySelector("userName")
+
+function savedID(event){
+    event.preventDefault();
+    const newID = idinput.value;
+    console.log(newID);
+    idinput.value = "";
+    const h1 = document.createElement("h1")
+    userName.innerText = `hello , ${newID}`;
+    localStorage.setItem("id", JSON.stringify(newID))
+}
+
+idForm.addEventListener("submit", savedID);
+
+const toDoForm = document.querySelector("#todo")
+const toDoInput = toDoForm.querySelector("input")
+const toDOList = document.getElementById("todo-list")
+
+const TODOS_KEY = "todos"
+
+let toDos = [];
+
+function savedTODOs(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
+}
+
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove()
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id))
+    savedTODOs();
+    
+}
+
+function paintToDo(newTodo){
+    const li = document.createElement("li")
+    li.id = newTodo.id;
+    const span = document.createElement("span")
+    span.innerText = newTodo.text;
+    const button = document.createElement("button")
+    button.innerText="x";
+    button.addEventListener("click", deleteToDo)
+    li.appendChild(span)
+    li.appendChild(button)
+    toDOList.appendChild(li)
+    savedTODOs()
+}
+
+function handleToDOSumbit(event){
+    event.preventDefault();
+    const newToDO = toDoInput.value;
+    toDoInput.value = ""
+    const newToDoObj = {
+        text : newToDO,
+        id : Date.now(),
+    }
+    toDos.push(newToDoObj)
+    paintToDo(newToDoObj)
+    savedTODOs()
+}
+
+toDoForm.addEventListener("submit", handleToDOSumbit);
+
+
+const saveToDos = localStorage.getItem(TODOS_KEY);
+
+if(saveToDos !== null){
+    let parsedToDos = JSON.parse(saveToDos)
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
+
